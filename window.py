48526e87f2fc96d3756640f6be1cb8857081bff8
Nicolas Vanz <nicavanz@hotmail.com>
2021-06-21 21:23:52 -0300

    bugfix: rearrange window acording to zoom

--- window.py
+++ window.py
@@ -1,76 +1,188 @@
 from abc import abstractclassmethod
 import tkinter as tk
+from tk_adaptations import *
+from shapes import *
+import windows_interfaces
 import re
+import math
 
-class GraphicObject:
-	def __init__(self, canvas, name, is_closed, args):
-		self.canvas = canvas
-		self.name = name
-		self.is_closed = is_closed
-		self.coordinates = args
-	
-	#@abstractclassmethod
-	def draw(self):
-		if len(self.coordinates) == 1:
-			c = self.coordinates[0] 
-			xvp = c[0]
-			yvp = c[1] 
-			self.canvas.create_line(xvp, yvp, xvp + 1, yvp)
-		else:
-			for i in range(len(self.coordinates) - 1):
-				c = self.coordinates[i]
-				d = self.coordinates[i + 1]
-				xvp1 = c[0]
-				yvp1 = c[1] 
-				xvp2 = d[0]
-				yvp2 = d[1]
-				self.canvas.create_line(xvp1, yvp1, xvp2, yvp2)
-			if self.is_closed:
-				c = self.coordinates[-1]
-				d = self.coordinates[0]
-				xvp1 = c[0]
-				yvp1 = c[1]
-				xvp1 = d[0]
-				yvp1 = d[1]
-				self.canvas.create_line(xvp1, yvp1, xvp2, yvp2)
 
-class Frame(tk.Frame):
-	def __init__(self, master, padx = 10, pady = 10, *args, **kwargs):
-		super().__init__(master = master, padx = padx, pady = pady, *args, **kwargs)
-
-class Label(tk.Label):
-	def __init__(self, master, padx = 10, pady = 10, *args, **kwargs):
-		super().__init__(master = master, padx = padx, pady = pady, *args, **kwargs)
-
-class Button(tk.Button):
-	def __init__(self, master, padx = 10, pady = 10, *args, **kwargs):
-		super().__init__(master = master, padx = padx, pady = pady, *args, **kwargs)
-
-class NewObjectWindow(tk.Toplevel):
-	def __init__(self, mainwindow):
-		self.main_window = mainwindow
-
-	def show(self):
-		# create second window
+class MainWindow(windows_interfaces.MainWindowInterface):
+	def __init__(self):
 		super().__init__()
-		self.title("New Object")
+
+		# overwrite button functions 
+		# (it takes initialy from MainWindowInterface functions)
+		self.button_in.configure(   command = self.__zoom_in)
+		self.button_out.configure(  command = self.__zoom_out)		
+		self.button_up.configure(   command = self.__move_up)		
+		self.button_down.configure( command = self.__move_down)
+		self.button_left.configure( command = self.__move_left)
+		self.button_right.configure(command = self.__move_right)
+
+		self.canvas.draw()
+
+	def __zoom(self, delta):
+		rangex = (self.canvas.maxx - self.canvas.minx) / 2
+		rangey = (self.canvas.maxy - self.canvas.miny) / 2
+	
+		# get center coordinates
+		midlex = (self.canvas.maxx + self.canvas.minx) / 2
+		midley = (self.canvas.maxy + self.canvas.miny) / 2
+
+		# rearange window position
+		self.canvas.minx = midlex - (rangex * 1 / delta)
+		self.canvas.miny = midley - (rangey * 1 / delta)
+		self.canvas.maxx = midlex + (rangex * 1 / delta)
+		self.canvas.maxy = midley + (rangey * 1 / delta)
+
+		# update image scale
+		self.canvas.imgscale *= delta
+		# redraw scene
+		self.canvas.draw()
+
+	def __zoom_in(self):
+		self.__zoom(self.canvas.delta_zoom)
 		
-		# we shouldn't place elements directly in root
-		self.mainframe = Frame(self)
-		self.mainframe.grid(row = 0, column = 0)
+	def __zoom_out(self):
+		self.__zoom(1 / self.canvas.delta_zoom)
+	
+	def __move_up(self):
+		self.canvas.miny += self.canvas.delta_move
+		self.canvas.maxy += self.canvas.delta_move
+		self.canvas.draw()
+		
+	def __move_down(self):
+		self.canvas.miny -= self.canvas.delta_move
+		self.canvas.maxy -= self.canvas.delta_move
+		self.canvas.draw()
 
-		# build user interface
-		self.__init_ui()
+	def __move_left(self):
+		self.canvas.minx -= self.canvas.delta_move
+		self.canvas.maxx -= self.canvas.delta_move
+		self.canvas.draw()
 
-	def hide(self):
-		# destroy the window
-		super().destroy()
+	def __move_right(self):
+		self.canvas.minx += self.canvas.delta_move
+		self.canvas.maxx += self.canvas.delta_move
+		self.canvas.draw()
 
-	def __create_object(self):
+
+class Viewport(tk.Canvas):
+	def __init__(self, master, mainwindow):
+		self.mainwindow = mainwindow
+
+		# viewport size
+		self.width = 500
+		self.height = 500
+
+		# navigation coefitients
+		self.delta_move = 30
+		self.delta_zoom = 1.4
+
+		# scene scale
+		self.imgscale = 1
+
+		# window coordinates
+		self.minx = -self.width/2
+		self.miny = -self.height/2
+		self.maxx = self.width/2
+		self.maxy = self.height/2
+
+		self.graphicObjects = []
+
+		self.graphic_object_creator = GraphicObjectCreator(
+			self
+		)
+
+		super().__init__(
+			master = master,
+			bg = "white",
+			width = self.width,
+			height = self.height,
+		)
+
+		self.__create_axis()
+
+	def transform(self, coords):
+		transformed = []
+		kx = self.width
+		ky = self.height
+		tx = (self.maxx-self.minx)
+		ty = (self.maxy-self.miny)
+		for c in coords:
+			x, y = c
+
+			transformed.append(
+				(
+					(     (x - self.minx) / tx)  * kx,
+					(1 - ((y - self.miny) / ty)) * ky
+				)
+			)
+
+		return transformed
+	
+	def __create_axis(self):
+		axisx = AxisX(
+			canvas = self,
+			name = "axis-x",
+			coords = [
+				(-math.inf, 0),
+				(math.inf, 0)
+			],
+			fill = "red"
+		)
+		axisy = AxisY(
+			canvas = self,
+			name = "axis-y",
+			coords = [
+				(0, -math.inf),
+				(0, math.inf)
+			],
+			fill = "green"
+		)
+
+		# add axis to the objects list to display them
+		self.graphicObjects.append(axisx)
+		self.graphicObjects.append(axisy)
+		
+		self.draw()
+	
+	def create_object(self, name, coords, is_closed = False, fill = "black"):
+		# create new graphic object
+		newGraphicObject = self.graphic_object_creator.create(
+			name,
+			coords,
+			is_closed,
+			fill
+		)
+
+		# add new object to the list
+		self.graphicObjects.append(newGraphicObject)
+
+		# draw new object
+		newGraphicObject.draw()
+
+		return newGraphicObject
+
+	def draw(self):
+		self.delete("all")
+		for i in self.graphicObjects:
+			i.draw()
+	
+
+
+class NewObjectWindow(windows_interfaces.NewObjectWindowInterface):
+	def submit(self):
 		# get object name
 		name = self.ent_name.get()
+		
+		if (not name):
+			print("No name specified")
+			return
+		
 		# ger Check Button value
-		chkButton = self.chkValue.get()
+		is_closed = self.chkValue.get()
 		# get coordinates list
 		coord = list(
 			map(
@@ -81,216 +193,19 @@ class NewObjectWindow(tk.Toplevel):
 				)
 			)
 		)
-		canvas = self.main_window.canvas
-		coordinates = []
-		for c in coord:
-			x = c[0]
-			y = c[1]
-			xvp = ((x - canvas.minx)/(canvas.maxx-canvas.minx)) * (canvas.vpmaxx)
-			yvp = (1 - (y - canvas.miny)/(canvas.maxy-canvas.miny)) * (canvas.vpmaxy)
-			coordinates.append((xvp, yvp))
-		
-		newGraphicObject = GraphicObject(
-			canvas,
+
+		if (not coord):
+			print("No coordinates specified")
+			return
+
+		obj = self.mainwindow.canvas.create_object(
 			name,
-			chkButton,
-			coordinates
+			coord,
+			is_closed,
 		)
 
-		self.main_window.graphicObjects.append(newGraphicObject)
-		newGraphicObject.draw()
-		self.main_window.lst_objNames.insert("end", name)
-
-	def __init_ui(self):
-		# build top frame
-		self.__build_fr_top()
-		# build bottom frame
-		self.__build_fr_bottom()
-
-	def __build_fr_top(self):
-		# create elements
-		self.fr_top   = Frame(self.mainframe)
-		self.lb_name  = Label(self.fr_top, text = "Name")
-		self.lb_coord = Label(self.fr_top, text = "Coordinates")
-		self.lb_isClosed = Label(self.fr_top, text = "It's a closed object")
-		self.ent_name  = tk.Entry(self.fr_top)
-		self.ent_coord = tk.Entry(self.fr_top)
-		self.chkValue = tk.BooleanVar() 
-		self.checkB_isClosed = tk.Checkbutton(self.fr_top, variable = self.chkValue)
-
-	
-		# positioning elements
-		self.fr_top.grid(    row = 0, column = 0)
-		self.lb_name.grid(   row = 0, column = 0)
-		self.ent_name.grid(  row = 1, column = 0)
-		self.lb_coord.grid(  row = 2, column = 0)
-		self.ent_coord.grid( row = 3, column = 0)
-		self.lb_isClosed.grid(    row = 4, column = 0)
-		self.checkB_isClosed.grid(row = 5, column = 0)
-	
-	def __build_fr_bottom(self):
-		# create bottom frame
-		self.fr_bottom = Frame(self.mainframe)
-
-		# create buttons
-		self.btn_ok = Button(
-			self.fr_bottom,
-			text = "Ok",
-			command = self.__create_object
-		)
-		self.btn_cancel = Button(
-			self.fr_bottom,
-			text = "Cancel",
-			command = self.hide
-		)
-
-		# positioning elements
-		self.fr_bottom.grid( row = 2, column = 0)
-		self.btn_ok.grid(    row = 0, column = 0)
-		self.btn_cancel.grid(row = 0, column = 1)
-
-class MainWindow(tk.Tk):
-	def __init__(self):
-		# create window
-		super().__init__()
-
-		# list of objects
-		self.graphicObjects = []
-
-		# we shouldn't place elements directly in root
-		self.mainframe = Frame(self)
-		self.mainframe.grid(row = 0, column = 0)
-
-		self.__build_interface()
-		self.__build_list_box()
-
-		self.new_object_window = NewObjectWindow(self)
-
-		self.canvas = Viewport(self.mainframe)
-		self.canvas.grid(row = 0, column = 1)
-	
-	def __build_list_box(self):
-		# create elements
-		self.fr_list_box = Frame(self.mainframe)
-		self.lb_objNames = Label(self.fr_list_box, text = "Object Names")
-		self.lst_objNames = tk.Listbox(self.fr_list_box)
-
-		# positioning elements
-		self.fr_list_box.grid( row = 0, column = 0)
-		self.lb_objNames.grid( row = 0, column = 0)
-		self.lst_objNames.grid(row = 1, column = 0)
-		
-
-	def __move(self, xam, yam):
-		canvas = self.canvas
-		for obj in canvas.find_all():
-			canvas.move(obj, xam, yam)
-
-	def __move_up(self):
-		self.__move(0, self.canvas.delta_move)
-		self.canvas.miny += self.canvas.delta_move
-		self.canvas.maxy += self.canvas.delta_move
-
-	def __move_down(self):	
-		self.__move(0, -self.canvas.delta_move)
-		self.canvas.miny -= self.canvas.delta_move
-		self.canvas.maxy -= self.canvas.delta_move
-
-	def __move_left(self):
-		self.__move(self.canvas.delta_move, 0)
-		self.canvas.minx -= self.canvas.delta_move
-		self.canvas.maxx -= self.canvas.delta_move
-
-	def __move_right(self):
-		self.__move(-self.canvas.delta_move, 0)
-		self.canvas.minx += self.canvas.delta_move
-		self.canvas.maxx += self.canvas.delta_move
-
-	def __build_interface(self):
-		self.frame_commands = Frame(self.mainframe)
-		self.frame_commands.grid(row = 1, column = 0)
-
-		self.button_newobject = Button(
-			self.frame_commands,
-			text = "New Object",
-			command = self.new_object
-		)
-		self.button_newobject.grid(row = 0, column = 0)
-
-		self.frame_zoom = Frame(self.frame_commands)
-		self.frame_zoom.grid(row = 1, column = 0)
-		self.button_in = Button(
-			self.frame_zoom,
-			text = "In" # ,command = self.canvas.scale
-		)
-		self.button_out = Button(
-			self.frame_zoom,
-			text = "Out" # ,command = self.canvas.scale
-		)
-		self.button_in.grid(row = 0, column = 0)
-		self.button_out.grid(row = 0, column = 1)
-
-		self.frame_arrows = Frame(self.frame_commands)
-		self.frame_arrows.grid(row = 2, column = 0)
-
-
-		self.button_up = tk.Button(
-			self.frame_arrows,
-			text = "^",
-			command = self.__move_up
-		)
-
-		self.button_down = tk.Button(
-			self.frame_arrows,
-			text = "v",
-			command = self.__move_down
-		)
-		self.button_left = tk.Button(
-			self.frame_arrows,
-			text = "<",
-			command = self.__move_left
-		)
-		self.button_right = tk.Button(
-			self.frame_arrows,
-			text = ">",
-			command = self.__move_right
-		)
-
-		self.button_up.grid(row=0, column = 0, columnspan = 2)
-		self.button_left.grid(row=1, column = 0)
-		self.button_right.grid(row = 1, column = 1)
-		self.button_down.grid(row=2, column = 0, columnspan = 2)
-
-	def new_object(self):
-		self.new_object_window.show()
-
-class Viewport(tk.Canvas):
-	def __init__(
-		self,
-		master, 
-		width = 500, 
-		height = 500, 
-		delta_move = 10, 
-		delta_zoom = 1.3
-		):
-
-		self.width = width
-		self.height = height
-		self.delta_move = delta_move
-		self.delta_zoom = delta_zoom
-		self.minx = -self.width/2
-		self.miny = -self.height/2
-		self.maxx = self.width/2
-		self.maxy = self.height/2
-		self.vpmaxx = self.width
-		self.vpmaxy = self.height
-
-		super().__init__(
-			master = master,
-			bg = "white",
-			width = self.width,
-			height = self.height,
-		)
+		# update object names list box
+		self.mainwindow.lst_objNames.insert("end", obj.name)
 
 def test():
 	root = MainWindow()
---
+++ windows_interfaces.py
@@ -0,0 +1,185 @@
+import tkinter as tk
+from shapes import *
+from tk_adaptations import *
+
+class NewObjectWindowInterface(tk.Toplevel):
+	def __init__(self, mainwindow):
+		self.mainwindow = mainwindow
+
+	@abstractclassmethod
+	def submit(self):
+		pass
+
+	def show(self):
+		# create second window
+		super().__init__()
+		self.title("New Object")
+		
+		# we shouldn't place elements directly in root
+		self.mainframe = Frame(self)
+		self.mainframe.grid(row = 0, column = 0)
+
+		# build user interface
+		self.__init_ui()
+
+	def hide(self):
+		# destroy the window
+		super().destroy()
+
+	def __init_ui(self):
+		# build top frame
+		self.__build_fr_top()
+		# build bottom frame
+		self.__build_fr_bottom()
+
+	def __build_fr_top(self):
+		# create elements
+		self.fr_top   = Frame(self.mainframe)
+		self.lb_name  = Label(self.fr_top, text = "Name")
+		self.lb_coord = Label(self.fr_top, text = "Coordinates")
+		self.lb_isClosed = Label(self.fr_top, text = "Is a closed object")
+		self.ent_name  = tk.Entry(self.fr_top)
+		self.ent_coord = tk.Entry(self.fr_top)
+		self.chkValue = tk.BooleanVar() 
+		self.checkB_isClosed = tk.Checkbutton(self.fr_top, variable = self.chkValue)
+
+	
+		# positioning elements
+		self.fr_top.grid(         row = 0, column = 0)
+		self.lb_name.grid(        row = 0, column = 0)
+		self.ent_name.grid(       row = 1, column = 0)
+		self.lb_coord.grid(       row = 2, column = 0)
+		self.ent_coord.grid(      row = 3, column = 0)
+		self.lb_isClosed.grid(    row = 4, column = 0)
+		self.checkB_isClosed.grid(row = 5, column = 0)
+	
+	def __build_fr_bottom(self):
+		# create bottom frame
+		self.fr_bottom = Frame(self.mainframe)
+
+		# create buttons
+		self.btn_ok = Button(
+			self.fr_bottom,
+			text = "Ok",
+			command = self.submit
+		)
+		self.btn_cancel = Button(
+			self.fr_bottom,
+			text = "Cancel",
+			command = self.hide
+		)
+
+		# positioning elements
+		self.fr_bottom.grid( row = 2, column = 0)
+		self.btn_ok.grid(    row = 0, column = 0)
+		self.btn_cancel.grid(row = 0, column = 1)
+
+class MainWindowInterface(tk.Tk):
+	def __init__(self):
+		import window
+		# create window
+		super().__init__()
+
+		# we shouldn't place elements directly in root
+		self.mainframe = Frame(self)
+		
+		self.new_object_window = window.NewObjectWindow(mainwindow = self)
+		self.canvas = window.Viewport(self.mainframe, mainwindow = self)
+
+		self.__init_ui()
+		
+		self.mainframe.grid(row = 0, column = 0)
+		self.canvas.grid(row = 0, column = 1)
+	
+	@abstractclassmethod
+	def __zoom_in(self):
+		pass
+
+	@abstractclassmethod
+	def __zoom_out(self):
+		pass
+
+	@abstractclassmethod
+	def __move_up(self):
+		pass
+
+	@abstractclassmethod
+	def __move_down(self):
+		pass
+
+	@abstractclassmethod
+	def __move_left(self):
+		pass
+
+	@abstractclassmethod
+	def __move_right(self):
+		pass
+
+	def __init_ui(self):
+		self.frame_left = Frame(self.mainframe)
+		self.frame_commands = Frame(self.frame_left)
+		self.frame_zoom = Frame(self.frame_commands)
+		self.frame_arrows = Frame(self.frame_commands)
+		self.fr_list_box = Frame(self.frame_left)
+		
+		self.lb_objNames = Label(self.fr_list_box, text = "Object Names")
+		self.lst_objNames = tk.Listbox(self.fr_list_box)
+
+		self.button_newobject = Button(
+			self.frame_commands,
+			text = "New Object",
+			command = self.__new_object
+		)
+
+		self.button_in = Button(
+			self.frame_zoom,
+			text = "In",
+			command = self.__zoom_in
+		)
+
+		self.button_out = Button(
+			self.frame_zoom,
+			text = "Out",
+			command = self.__zoom_out
+		)
+
+		self.button_up = tk.Button(
+			self.frame_arrows,
+			text = "^",
+			command = self.__move_up
+		)
+
+		self.button_down = tk.Button(
+			self.frame_arrows,
+			text = "v",
+			command = self.__move_down
+		)
+		self.button_left = tk.Button(
+			self.frame_arrows,
+			text = "<",
+			command = self.__move_left
+		)
+		self.button_right = tk.Button(
+			self.frame_arrows,
+			text = ">",
+			command = self.__move_right
+		)		
+
+		# positioning elements
+		self.frame_left.grid(      row = 0, column = 0)
+		self.frame_commands.grid(  row = 1, column = 0)
+		self.button_newobject.grid(row = 0, column = 0)
+		self.frame_zoom.grid(      row = 1, column = 0)
+		self.button_in.grid(       row = 0, column = 0)
+		self.button_out.grid(      row = 0, column = 1)
+		self.frame_arrows.grid(    row = 2, column = 0)
+		self.fr_list_box.grid(     row = 0, column = 0)
+		self.lb_objNames.grid(     row = 0, column = 0)
+		self.lst_objNames.grid(    row = 1, column = 0)
+		self.button_up.grid(       row = 0, column = 0, columnspan = 2)
+		self.button_left.grid(     row = 1, column = 0)
+		self.button_right.grid(    row = 1, column = 1)
+		self.button_down.grid(     row = 2, column = 0, columnspan = 2)
+
+	def __new_object(self):
+		self.new_object_window.show()
